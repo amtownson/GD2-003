a7ac848dbe90561aabfa756350bad16f61309fdc
amtownson <59401739+amtownson@users.noreply.github.com>
2021-01-29 17:14:51 -0500

    New Stuff

---
+++ ArcadeStart.js
@@ -0,0 +1,14 @@
+console.log("Arcade");
+
+var CB = document.getElementById("CoinBlock");
+var HB = document.getElementById("HeartBlock");
+var P1B = document.getElementById("Player1Block");
+var P2B = document.getElementById("Player2Block");
+var Game = document.getElementById("Game");
+
+CB.addEventListener("click",function () {CB.classList.toggle("Hide");}, false);
+HB.addEventListener("click", function() {HB.classList.toggle("Hide");}, false);	
+
+P1B.addEventListener("click",function () {P2B.classList.toggle("Hide");}, false);
+P2B.addEventListener("click", function() {P1B.classList.toggle("Hide");}, false);	
+
--- Hover.js
+++
@@ -1,9 +0,0 @@
-// function ChangeColourF() {
-// document.getElementById("ChangeColourJS").style.backgroundColor= "purple";
-// return false;
-// };
-
-
-var ChangeColour = document.GetElementById("ChangeColourJS");
-
-ChangeColour.addEventListener("click", function() {ChangeColour.classlist.toggle("NewColour");}, false);
