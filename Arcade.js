8e4fc1a0f7521da4c3229523342cc5e0d02f5673
amtownson <59401739+amtownson@users.noreply.github.com>
2021-02-05 15:42:36 -0500

    BiggerArcade

--- Arcade.js
+++ Arcade.js
@@ -12,3 +12,34 @@ HB.addEventListener("click", function() {HB.classList.toggle("Hide");}, false);
 P1B.addEventListener("click",function () {P2B.classList.toggle("Hide");}, false);
 P2B.addEventListener("click", function() {P1B.classList.toggle("Hide");}, false);	
 
+
+
+
+// Player moving
+
+// let player1 = document.querySelector(".Player1Block");
+// let moveBy = 10;
+
+// window.addEventListener("load", () => {
+// 	player1.style.position = "absolute";
+// 	player1.style.left = 0;
+// 	player1.style.top = 0; 
+// });
+
+// window.addEventListener("keydown", (e) => {
+// 	switch (e.key) {
+// 		case "ArrowLeft":
+// 			player1.style.left = parseInt(player1.style.left) - moveBy + "px";
+// 			break;
+// 		case "ArrowRight":
+// 			player1.style.left = parseInt(player1.style.left) + moveBy + "px";
+// 			break;
+// 		case "ArrowUp":
+// 			player1.style.top = parseInt(player1.style.top) - moveBy + "px";
+// 			break;
+// 		case "ArrowDown":
+// 			player1.style.top = parseInt(player1.style.top) + moveBy + "px";
+// 			break;
+
+// 	}
+// });
