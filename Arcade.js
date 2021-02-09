2f5b5121249c4959122dde13bc370c1c9f45f314
amtownson <59401739+amtownson@users.noreply.github.com>
2021-02-08 21:08:09 -0500

    More Interaction
    Start screen complete, arcade wip

--- Arcade.js
+++ Arcade.js
@@ -1,45 +1,177 @@
 console.log("Arcade");
 
-var CB = document.getElementById("CoinBlock");
-var HB = document.getElementById("HeartBlock");
-var P1B = document.getElementById("Player1Block");
-var P2B = document.getElementById("Player2Block");
+// START SCREEN
+
+// var CB = document.getElementById("CoinBlock");
+// var HB = document.getElementById("HeartBlock");
+var PB1 = document.getElementById("PlayerBlock1");
+var PB2 = document.getElementById("PlayerBlock2");
+var door = document.getElementById("Enter");
+var GameTitle = document.getElementById("GameTitle");
+var Select = document.getElementById("Select");
+var controls1 = document.getElementById("Controls1");
+var controls2 = document.getElementById("Controls2");
+var	keyCodes = { left: 37, leftA: 65, up: 38, upW: 87, right: 39, rightD: 68, down: 40, downS: 83, enter: 13 },
+	keys = [];
 
 
-CB.addEventListener("click",function () {CB.classList.toggle("Hide");}, false);
-HB.addEventListener("click", function() {HB.classList.toggle("Hide");}, false);	
 
-P1B.addEventListener("click",function () {P2B.classList.toggle("Hide");}, false);
-P2B.addEventListener("click", function() {P1B.classList.toggle("Hide");}, false);	
+PB1.addEventListener("click", P1Start);
+PB2.addEventListener("click", P2Start);	
+
+function P1Start(){
+	PB2.classList.replace("PlayerBlock2", "Hide");
+	false;
+	Move1();
+	door.style.display = "block";
+	Select.style.display = "none";
+	GameTitle.style.display = "block";
+	controls1.style.display = "block";
+};
+
+function P2Start(){
+	PB1.classList.replace("PlayerBlock1", "Hide");
+	false;
+	Move2();
+	door.style.display = "block";
+	Select.style.display = "none";
+	GameTitle.style.display = "block";
+	controls2.style.display = "block";
+};
+
+window.addEventListener("load", () => {
+	PB1.style.position = "absolute";
+	PB1.style.left = 0;
+	PB1.style.top = 0; 
+	PB2.style.position = "absolute";
+	PB2.style.left = 0;
+	PB2.style.top = 0; 
+});
+
+function Move1 (){
+	
+	window.addEventListener("keydown", function (e) {
+	    keys[e.keyCode] = true;
+	});
+	
+	window.addEventListener("keyup", function (e) {
+	    keys[e.keyCode] = false;
+	});
+	
+	setInterval(function () {
+	    
+	    var x = parseInt(PB1.style.left, 10),
+	        y = parseInt(PB1.style.top, 10),
+	       winWidMar = window.innerWidth - 200,
+	       winHeiMar = window.innerHeight - 500;
+	    
+  		  if (keys[keyCodes.left] || keys[keyCodes.leftA]) {
+	        x -= 1;
+	    } else if (keys[keyCodes.right] || keys[keyCodes.rightD]) {
+	        x += 1;
+	    };
+	
+	    if (keys[keyCodes.up] || keys[keyCodes.upW ]) {
+	        y -= 1;
+	    } else if (keys[keyCodes.down] || keys[keyCodes.downS]) {
+	        y += 1;
+	    };
+	    
+	    PB1.style.left = x + "px";
+	    PB1.style.top = y + "px";
+
+	    if (x<0 || x>0) {
+	    	controls1.style.display = "none";
+	    }else if(y<0 || y>0){
+	    	controls1.style.display = "none";
+	    }
+
+	    // go off screen
+
+		if (x< -350){
+		PB1.style.left = winWidMar +"px";
+		} else if (x>winWidMar){
+		PB1.style.left= -350 +"px";
+		};
+
+		if (y< -790){
+		PB1.style.top= winHeiMar +"px";
+		} else if (y> winHeiMar){
+		PB1.style.top= -790 + "px";	
+		};
+
+		// Enter the door
+		
+		if (x>936 && x<1237 && y> -430 && y<275 && keys[keyCodes.enter]){
+		window.location.href = "Arcade.html"};
+
+		var player = "player1";
+		localStorage.setItem("player", player);
+
+	}, 1/30);
+};
+
+
+function Move2 (){
+
+	
+	window.addEventListener("keydown", function (e) {
+	    keys[e.keyCode] = true;
+	});
+	
+	window.addEventListener("keyup", function (e) {
+	    keys[e.keyCode] = false;
+	});
+	
+	setInterval(function () {
+	    
+	    var x = parseInt(PB2.style.left, 10),
+	        y = parseInt(PB2.style.top, 10),
+	       winWidMar = window.innerWidth - 490,
+	       winHeiMar = window.innerHeight - 500;
+	    
+   		 if (keys[keyCodes.left] || keys[keyCodes.leftA]) {
+	        x -= 1;
+	    } else if (keys[keyCodes.right] || keys[keyCodes.rightD]) {
+	        x += 1;
+	    };
+	
+	    if (keys[keyCodes.up] || keys[keyCodes.upW ]) {
+	        y -= 1;
+	    } else if (keys[keyCodes.down] || keys[keyCodes.downS]) {
+	        y += 1;
+	    };
+	    
+	    PB2.style.left = x + "px";
+	    PB2.style.top = y + "px";
+
+	    if (x<0 || x>0) {
+	    	controls2.style.display = "none";
+	    }else if(y<0 || y>0){
+	    	controls2.style.display = "none";
+	    }
+
+		if (x< -640){
+		PB2.style.left = winWidMar +"px";
+		} else if (x>winWidMar){
+		PB2.style.left= -640 +"px";
+		};
+
+		if (y< -790){
+		PB2.style.top= winHeiMar +"px";
+		} else if (y> winHeiMar){
+		PB2.style.top= -790 + "px";	
+		};
+
+		if (x>657 && x<1000 && y> -418 && y<267 && keys[keyCodes.enter]){
+		window.location.href = "Arcade.html"};
+	    
+		var player = "player2";
+		localStorage.setItem("player", player);
+
+	}, 1/30);
+};
 
 
 
 
-// Player moving
-
-// let player1 = document.querySelector(".Player1Block");
-// let moveBy = 10;
-
-// window.addEventListener("load", () => {
-// 	player1.style.position = "absolute";
-// 	player1.style.left = 0;
-// 	player1.style.top = 0; 
-// });
-
-// window.addEventListener("keydown", (e) => {
-// 	switch (e.key) {
-// 		case "ArrowLeft":
-// 			player1.style.left = parseInt(player1.style.left) - moveBy + "px";
-// 			break;
-// 		case "ArrowRight":
-// 			player1.style.left = parseInt(player1.style.left) + moveBy + "px";
-// 			break;
-// 		case "ArrowUp":
-// 			player1.style.top = parseInt(player1.style.top) - moveBy + "px";
-// 			break;
-// 		case "ArrowDown":
-// 			player1.style.top = parseInt(player1.style.top) + moveBy + "px";
-// 			break;
-
-// 	}
-// });
---
+++ ArcadeInside.js
@@ -0,0 +1,144 @@
+var PB1IN = document.getElementById("PlayerBlockIN1");
+var PB2IN = document.getElementById("PlayerBlockIN2");
+var player = localStorage.getItem("player");
+var arcadeBody = document.getElementById("arcadeBody");
+var	keyCodes = { left: 37, leftA: 65, up: 38, upW: 87, right: 39, rightD: 68, down: 40, downS: 83, enter: 13 },
+	keys = [];
+var arcadex = parseInt(arcadeBody.style.left, 10);
+var arcadey = parseInt(arcadeBody.style.top, 10);
+var winWidMar = window.innerWidth - 45;
+var winHeiMar = window.innerHeight - 410;
+var WinHalfx = window.innerWidth /2;
+var WinHalfy = window.innerHeight /2;
+
+window.addEventListener("load", () => {
+	PB1IN.style.position = "absolute";
+	PB1IN.style.left = 0;
+	PB1IN.style.top = 0; 
+	PB2IN.style.position = "absolute";
+	PB2IN.style.left = 0;
+	PB2IN.style.top = 0; 
+	arcadeBody.style.position = "absolute";
+	arcadeBody.style.left = 0;
+	arcadeBody.style.top = 0;
+	if(player == "player1"){
+		MoveIN1();
+		PB2IN.style.display = "none"
+	}else if(player == "player2")
+	{MoveIN2();
+	PB1IN.style.display = "none";
+	}
+});
+
+
+function MoveIN1 (){
+	
+	window.addEventListener("keydown", function (e) {
+	    keys[e.keyCode] = true;
+});
+	
+	window.addEventListener("keyup", function (e) {
+	    keys[e.keyCode] = false;
+	});
+	
+	setInterval(function () {
+	  
+	    var x = parseInt(PB1IN.style.left, 10),
+	        y = parseInt(PB1IN.style.top, 10);
+
+	    
+	       // moving
+	    
+   		 if (x> -57 && keys[keyCodes.left] && x>=WinHalfx || x> -57 && keys[keyCodes.leftA] && x>=WinHalfx) 
+	        {arcadex +=1;
+	    	x -=1;
+	    }else if (x> -57 && keys[keyCodes.left] || x> -57 && keys[keyCodes.leftA])
+	        {x -=1;};
+
+	    if (x<4100 && keys[keyCodes.right] && x>=WinHalfx || x<4100 && keys[keyCodes.rightD] && x>=WinHalfx) 
+	    	{arcadex -=1;
+	    	x +=1;
+	    }else if (x<4100 && keys[keyCodes.right]|| x<4100 && keys[keyCodes.rightD]) 
+	        {x +=1;};
+	
+
+	    if (y> -125 && keys[keyCodes.up] || y> -125 && keys[keyCodes.upW])
+	        {arcadey+=1;
+	    	y -=1;}
+	  
+	    if (y<1100 && keys[keyCodes.down]|| y<1100 && keys[keyCodes.downS]) 
+	        {arcadey-=1;
+	        y +=1;}
+
+	    	    // exit 
+
+		if (x>-125 && x<300 && y> -57 && y<150 && keys[keyCodes.enter]){
+		window.location.href = "index.html"};
+
+
+
+	    PB1IN.style.left = x + "px";
+	    PB1IN.style.top = y + "px";
+	    arcadeBody.style.left = arcadex + "px";
+	    arcadeBody.style.top = arcadey + "px";
+
+
+		}, 1/30);
+};
+    
+
+function MoveIN2 (){
+
+	window.addEventListener("keydown", function (e) {
+	    keys[e.keyCode] = true;
+
+	});
+	
+	window.addEventListener("keyup", function (e) {
+	    keys[e.keyCode] = false;
+	});
+	
+	setInterval(function () {
+	    
+	    var x = parseInt(PB2IN.style.left, 10),
+	        y = parseInt(PB2IN.style.top, 10);
+	    
+	       // moving
+	    
+   		 if (x> -57 && keys[keyCodes.left] && x>=WinHalfx || x> -57 && keys[keyCodes.leftA] && x>=WinHalfx) 
+	        {arcadex +=1;
+	    	x -=1;
+	    }else if (x> -57 && keys[keyCodes.left] || x> -57 && keys[keyCodes.leftA])
+	        {x -=1;};
+
+	    if (x<4100 && keys[keyCodes.right] && x>=WinHalfx || x<4100 && keys[keyCodes.rightD] && x>=WinHalfx) 
+	    	{arcadex -=1;
+	    	x +=1;
+	    }else if (x<4100 && keys[keyCodes.right]|| x<4100 && keys[keyCodes.rightD]) 
+	        {x +=1;};
+	
+
+	    if (y> -125 && keys[keyCodes.up] || y> -125 && keys[keyCodes.upW])
+	        {arcadey+=1;
+	    	y -=1;}
+	  
+	    if (y<1100 && keys[keyCodes.down]|| y<1100 && keys[keyCodes.downS]) 
+	        {arcadey-=1;
+	        y +=1;}
+
+	    
+	    PB2IN.style.left = x + "px";
+	    PB2IN.style.top = y + "px";
+	    arcadeBody.style.left = arcadex + "px";
+	    arcadeBody.style.top = arcadey + "px";
+
+	   // exit 
+
+		if (x>-125 && x<300 && y> -57 && y<150 && keys[keyCodes.enter]){
+		window.location.href = "index.html"
+		console.log("working")};
+
+
+		}, 1/30);
+};
+
